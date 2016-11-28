fc4e116f59210ea79accf156244333373951b1c1
Cristian Pallarés <cpallares@inbenta.com>
2016-11-28 13:06:38 +0100

    Wrap code with IIFE

--- src/index.js
+++ src/index.js
@@ -14,26 +14,28 @@ export default function (output) {
     ${output}
 
     if (module.hot) {
-      var api = require('vue-hot-reload-api')
+      (function () {
+        var api = require('vue-hot-reload-api')
 
-      // make the API aware of the Vue that you are using.
-      // also checks compatibility.
-      api.install(require('vue'), false);
+        // make the API aware of the Vue that you are using.
+        // also checks compatibility.
+        api.install(require('vue'), false);
 
-      // compatibility can be checked via api.compatible after installation
-      if (!api.compatible) {
-        throw new Error(
-          'vue-hot-reload-api is not compatible with the version of Vue you are using.'
-        );
-      }
+        // compatibility can be checked via api.compatible after installation
+        if (!api.compatible) {
+          throw new Error(
+            'vue-hot-reload-api is not compatible with the version of Vue you are using.'
+          );
+        }
 
-      module.hot.accept();
+        module.hot.accept();
 
-      if (!module.hot.data) {
-        api.createRecord(${hotId}, module.exports.default);
-      } else {
-        api.rerender(${hotId}, module.exports.default);
-      }
+        if (!module.hot.data) {
+          api.createRecord(${hotId}, module.exports.default);
+        } else {
+          api.rerender(${hotId}, module.exports.default);
+        }
+      })();
     }
   `;
 }
