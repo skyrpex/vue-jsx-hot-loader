7d610237b1f2af5f9046164048e3a4f32d01959d
skyrpex <skyrpex@gmail.com>
2017-03-25 12:06:12 +0100

    Add test playground

---
+++ tests/webpack/src/component.jsx
@@ -0,0 +1,28 @@
+export default {
+  name: 'MyJsxComponent',
+  data() {
+    return {
+      name: 'world',
+    };
+  },
+  computed: {
+    test() {
+      return this.name.split('l');
+    },
+    test2() {
+      return this.name.split('l');
+    },
+  },
+  methods: {
+    toggleName() {
+      this.name = this.name === 'world' ? 'alls' : 'world';
+    },
+  },
+  // eslint-disable-next-line no-unused-vars
+  render(h) {
+    return <div>
+      <span>Hello, {this.name}!!</span>
+      <button onClick={this.toggleName}>Toggle!!</button>
+    </div>;
+  },
+};
