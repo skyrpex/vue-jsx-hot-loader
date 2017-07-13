20bb0609da99a1829f07895344ef19bc80806d08
Cristian Pallarés <cpallares@inbenta.com>
2017-07-13 23:00:19 +0200

    Disable rerender since it looks broken for JSX

--- src/api.js
+++ src/api.js
@@ -1,42 +1,18 @@
-const Vue = require('vue');
 const _ = require('lodash');
 const api = require('vue-hot-reload-api');
-const serialize = require('serialize-javascript');
 
-// We'll store here the serialized components.
-// The cache will be used to decide whenever
-// a reload or just a rerender is needed.
-const cache = {};
+const installApi = _.once((Vue) => {
+  // Make the API aware of the Vue that you are using.
+  // Also checks compatibility.
+  api.install(Vue, false);
 
-// https://github.com/yahoo/serialize-javascript/blob/adfee60681dd02b0c4ec73793ad4bb39bbff46ef/index.js#L15
-const IS_NATIVE_CODE_REGEXP = /\{\s*\[native code\]\s*\}/g;
-
-// Native objects aren't serializable by the 'serialize-javascript' package,
-// so we'll just transform it to strings.
-//
-// We'll use a local cache to ignore prevent transforming cyclic objects.
-const transformUnserializableProps = (item, localCache = null) => {
-  if (localCache == null) {
-    // eslint-disable-next-line no-param-reassign
-    localCache = [];
-  } else if (_.indexOf(localCache, item) !== -1) {
-    return null;
+  // Compatibility can be checked via api.compatible after installation.
+  if (!api.compatible) {
+    throw new Error(
+      'vue-hot-reload-api is not compatible with the version of Vue you are using.',
+    );
   }
-
-  if (_.isObject(item) || _.isArray(item)) {
-    localCache.push(item);
-    return _.mapValues(item, value => transformUnserializableProps(value, localCache));
-  }
-
-  if (item) {
-    const serializedItem = item.toString();
-    if (IS_NATIVE_CODE_REGEXP.test(serializedItem)) {
-      return serializedItem;
-    }
-  }
-
-  return item;
-};
+});
 
 const findComponent = ({ ctx, module }) => {
   // Babel did not transform modules
@@ -49,17 +25,8 @@ const findComponent = ({ ctx, module }) => {
   return module.exports.__esModule ? module.exports.default : module.exports;
 };
 
-module.exports = ({ ctx, module, hotId }) => {
-  // Make the API aware of the Vue that you are using.
-  // Also checks compatibility.
-  api.install(Vue, false);
-
-  // Compatibility can be checked via api.compatible after installation.
-  if (!api.compatible) {
-    throw new Error(
-      'vue-hot-reload-api is not compatible with the version of Vue you are using.',
-    );
-  }
+module.exports = ({ Vue, ctx, module, hotId }) => {
+  installApi(Vue);
 
   // Accept the hot replacement.
   module.hot.accept();
@@ -68,24 +35,11 @@ module.exports = ({ ctx, module, hotId }) => {
   // untransformed ES modules (env/es2015 preset with modules: false).
   const component = findComponent({ ctx, module });
 
-  // Serialize everything but the render function.
-  // We'll use it to decide if we need to reload or rerender.
-  const serialized = serialize(
-    transformUnserializableProps(_.omit(component, ['render'])),
-    { space: 0 },
-  );
-
   if (!module.hot.data) {
     // If no data, we need to create the record.
     api.createRecord(hotId, component);
-  } else if (cache[hotId] === serialized) {
-    // Rerender only since the component hasn't changed.
-    api.rerender(hotId, component);
   } else {
     // Reload the component.
     api.reload(hotId, component);
   }
-
-  // Save the serialized component to the cache.
-  cache[hotId] = serialized;
 };
