cfaac80958af80cb3d8c42201b51f5c2a3741222
Cristian Pallarés <cpallares@inbenta.com>
2017-11-21 11:35:44 +0100

    test: add more tests

--- tests/serialize.spec.js
+++ tests/serialize.spec.js
@@ -25,8 +25,17 @@ test('serializes regexps', () => {
   expect(serialize(/abc/)).toEqual('/abc/')
 })
 
+test('serializes dates', () => {
+  expect(serialize(new Date())).toBeDefined()
+  expect(serialize(new Date(2017))).not.toBe(serialize(new Date(2016)))
+})
+
+test('serializes arrays', () => {
+  expect(serialize(['string', /regexp/])).not.toBe(serialize(['string']))
+})
+
 test('serializes null objects', () => {
-  expect(serialize(Object.create(null))).toEqual('{}')
+  expect(serialize(Object.create(null))).toBe('{}')
 })
 
 test('serializes functions', () => {
@@ -35,4 +44,14 @@ test('serializes functions', () => {
 
 test('serialized functions are different between each other', () => {
   expect(serialize(() => 'value')).not.toBe(() => true)
+})
+
+test('serializes deep objects', () => {
+  expect(serialize({
+    string: 'string',
+    nested: {
+      boolean: true,
+      regexp: /regexp/,
+    },
+  })).toBe('{"string":"string","nested":{"boolean":true,"regexp":/regexp/}}')
 })
